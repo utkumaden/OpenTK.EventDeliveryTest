b472543a86ccd0bc913531b889e258e9b3fd99e4
H. Utku Maden <utkumaden@hotmail.com>
2025-12-18 23:33:29 +0300

    Add a test for virtuals like noggin suggested.

--- Program.cs
+++ Program.cs
@@ -24,9 +24,10 @@ enum EventType
     EventMax,
 }
 
-class MyEventArgs(EventType type) : EventArgs
+abstract class MyEventArgs(EventType type) : EventArgs
 {
     public EventType Type { get; } = type;
+    public abstract EventType TypeVirtual { get; }
 
     public static MyEventArgs GetRandom(Random random)
     {
@@ -57,22 +58,101 @@ class MyEventArgs(EventType type) : EventArgs
 
 // Dummy here is a dummy workload to sum together.
 
-class MyEventArgs1(float dummy) : MyEventArgs(EventType.Event1) { public float Dummy { get; } = dummy; }
-class MyEventArgs2(float dummy) : MyEventArgs(EventType.Event2) { public float Dummy { get; } = dummy; }
-class MyEventArgs3(float dummy) : MyEventArgs(EventType.Event3) { public float Dummy { get; } = dummy; }
-class MyEventArgs4(float dummy) : MyEventArgs(EventType.Event4) { public float Dummy { get; } = dummy; }
-class MyEventArgs5(float dummy) : MyEventArgs(EventType.Event5) { public float Dummy { get; } = dummy; }
-class MyEventArgs6(float dummy) : MyEventArgs(EventType.Event6) { public float Dummy { get; } = dummy; }
-class MyEventArgs7(float dummy) : MyEventArgs(EventType.Event7) { public float Dummy { get; } = dummy; }
-class MyEventArgs8(float dummy) : MyEventArgs(EventType.Event8) { public float Dummy { get; } = dummy; }
-class MyEventArgs9(float dummy) : MyEventArgs(EventType.Event9) { public float Dummy { get; } = dummy; }
-class MyEventArgs10(float dummy) : MyEventArgs(EventType.Event10) { public float Dummy { get; } = dummy; }
-class MyEventArgs11(float dummy) : MyEventArgs(EventType.Event11) { public float Dummy { get; } = dummy; }
-class MyEventArgs12(float dummy) : MyEventArgs(EventType.Event12) { public float Dummy { get; } = dummy; }
-class MyEventArgs13(float dummy) : MyEventArgs(EventType.Event13) { public float Dummy { get; } = dummy; }
-class MyEventArgs14(float dummy) : MyEventArgs(EventType.Event14) { public float Dummy { get; } = dummy; }
-class MyEventArgs15(float dummy) : MyEventArgs(EventType.Event15) { public float Dummy { get; } = dummy; }
-class MyEventArgs16(float dummy) : MyEventArgs(EventType.Event16) { public float Dummy { get; } = dummy; }
+class MyEventArgs1(float dummy) : MyEventArgs(EventType.Event1)
+{
+    public override EventType TypeVirtual => EventType.Event1;
+    public float Dummy { get; } = dummy;
+}
+class MyEventArgs2(float dummy) : MyEventArgs(EventType.Event2)
+{
+    public override EventType TypeVirtual => EventType.Event2;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs3(float dummy) : MyEventArgs(EventType.Event3)
+{
+    public override EventType TypeVirtual => EventType.Event3;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs4(float dummy) : MyEventArgs(EventType.Event4)
+{
+    public override EventType TypeVirtual => EventType.Event4;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs5(float dummy) : MyEventArgs(EventType.Event5)
+{
+    public override EventType TypeVirtual => EventType.Event5;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs6(float dummy) : MyEventArgs(EventType.Event6)
+{
+    public override EventType TypeVirtual => EventType.Event6;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs7(float dummy) : MyEventArgs(EventType.Event7)
+{
+    public override EventType TypeVirtual => EventType.Event7;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs8(float dummy) : MyEventArgs(EventType.Event8)
+{
+    public override EventType TypeVirtual => EventType.Event8;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs9(float dummy) : MyEventArgs(EventType.Event9)
+{
+    public override EventType TypeVirtual => EventType.Event9;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs10(float dummy) : MyEventArgs(EventType.Event10)
+{
+    public override EventType TypeVirtual => EventType.Event10;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs11(float dummy) : MyEventArgs(EventType.Event11)
+{
+    public override EventType TypeVirtual => EventType.Event11;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs12(float dummy) : MyEventArgs(EventType.Event12)
+{
+    public override EventType TypeVirtual => EventType.Event12;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs13(float dummy) : MyEventArgs(EventType.Event13)
+{
+    public override EventType TypeVirtual => EventType.Event13;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs14(float dummy) : MyEventArgs(EventType.Event14)
+{
+    public override EventType TypeVirtual => EventType.Event14;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs15(float dummy) : MyEventArgs(EventType.Event15)
+{
+    public override EventType TypeVirtual => EventType.Event15;
+    public float Dummy { get; } = dummy;
+}
+
+class MyEventArgs16(float dummy) : MyEventArgs(EventType.Event16)
+{
+    public override EventType TypeVirtual => EventType.Event16;
+    public float Dummy { get; } = dummy;
+}
+
 
 public class Benchmark
 {
@@ -107,6 +187,42 @@ public class Benchmark
         OnEvent += (sender, args) =>
         {
             sum += args.Type switch
+            {
+                EventType.Event1 => ((MyEventArgs1)args).Dummy,
+                EventType.Event2 => ((MyEventArgs2)args).Dummy,
+                EventType.Event3 => ((MyEventArgs3)args).Dummy,
+                EventType.Event4 => ((MyEventArgs4)args).Dummy,
+                EventType.Event5 => ((MyEventArgs5)args).Dummy,
+                EventType.Event6 => ((MyEventArgs6)args).Dummy,
+                EventType.Event7 => ((MyEventArgs7)args).Dummy,
+                EventType.Event8 => ((MyEventArgs8)args).Dummy,
+                EventType.Event9 => ((MyEventArgs9)args).Dummy,
+                EventType.Event10 => ((MyEventArgs10)args).Dummy,
+                EventType.Event11 => ((MyEventArgs11)args).Dummy,
+                EventType.Event12 => ((MyEventArgs12)args).Dummy,
+                EventType.Event13 => ((MyEventArgs13)args).Dummy,
+                EventType.Event14 => ((MyEventArgs14)args).Dummy,
+                EventType.Event15 => ((MyEventArgs15)args).Dummy,
+                EventType.Event16 => ((MyEventArgs16)args).Dummy,
+                _ => 0,
+            };
+        };
+
+        DispatchEvents();
+
+        Console.WriteLine($"Total = {sum}");
+
+        OnEvent = null;
+    }
+
+    [Benchmark]
+    public void MatchOnEnumVirtual()
+    {
+        float sum = 0f;
+
+        OnEvent += (sender, args) =>
+        {
+            sum += args.TypeVirtual switch
             {
                 EventType.Event1 => ((MyEventArgs1)args).Dummy,
                 EventType.Event2 => ((MyEventArgs2)args).Dummy,
